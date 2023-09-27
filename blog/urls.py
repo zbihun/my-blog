f497bb26e6e4a052d6522b75b49af7dcec9ea01f
zbihun <bighun.zoryana@gmail.com>
2023-09-27 18:08:55 +0300

    Add edit post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.post_list, name='post_list'),
-    path("post/<pk>/", views.post_detail, name='post_detail')
+    path("post/<pk>/", views.post_detail, name='post_detail'),
+    path("posts/new/", views.post_new, name='post_new'),
+    path("post/<pk>/edit/", views.post_edit, name='post_edit'),
 ]
